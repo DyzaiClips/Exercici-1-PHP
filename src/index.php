fefdad73532e3b27bb5ef05ce4d9d9ebbef56b78
Dan <dan.fernandez@cirvianum.cat>
2025-09-16 10:21:37 +0200

    Primer Commit del projecte Servidor

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>xxxxxxxxxxxxx</title>
+        <style>
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 90%;
+            }
+        </style>
+    </head>
+    <body>
+        <h1><?php echo "xxxxxxxxxxxxxxx"; ?></h1>
+        <p>
+        <?php
+            echo "Benvingut xxxxxxxxxxxxxxxxx";
+            echo "Today is " . date("l");
+            echo "Espero que t'agradi";
+        ?>
+        </p>
+        <img src="hello1.gif" alt="Hola" class="center">
+    </body>
+</html>
