27ca59322fab01d2ef58199b7e4103205fbe4ce0
Dan <dan.fernandez@cirvianum.cat>
2025-09-16 17:05:19 +0200

    8e Commit

---
+++ Exercici 1/06arrays01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Classic Array</h1>
+
+<?php
+
+    $fruits = ['apple', 'orange', 'melon'];
+    $fruits2 = array('apple','Orange','melon');
+    $a = [['Hola'],['Adeu'], ['HELLOW']]
+?>
+
+</body>
+
+</html>
