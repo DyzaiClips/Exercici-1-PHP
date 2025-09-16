59989c39a5439a5a4a06970d83486c3d899b4f2a
Dan <dan.fernandez@cirvianum.cat>
2025-09-16 17:15:46 +0200

    13e Commit

---
+++ Exercici 1/07arrays02.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Arrays Associatius</h1>
+
+<?php
+    $capitals = [
+        'Framce'=>'Paris',
+        'Spain'=>'Madrid',
+        'Italy'=>'Rome',
+        'German'=>'Berlin'
+    ]; 
+
+    $capitals['Greek']='Athenas';
+
+    $franceCapital = $capitals['France'];
+
+    echo "<pre>";
+    print_r( $capitals );
+    echo "</pre>";
+?>
+
+</body>
+
+</html>
