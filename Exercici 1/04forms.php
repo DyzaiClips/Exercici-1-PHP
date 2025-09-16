38e090fea3d4758a6486d2ef66832b042f79cc1a
Dan <dan.fernandez@cirvianum.cat>
2025-09-16 16:43:06 +0200

    5e Commit

---
+++ Exercici 1/04forms.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>EL meu primer Formulari</h1>
+
+    <form action="" method="get">
+        <p>
+            <label for="">Escriu un Numero</label>
+            <input type="text" name="numero">
+        </p>
+    </form>
+</body>
+</html>
