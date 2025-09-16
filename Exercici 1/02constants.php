17f52f411f486c2f924ac3c87900f56bacfde67f
Dan <dan.fernandez@cirvianum.cat>
2025-09-16 16:35:49 +0200

    Commit 2

---
+++ Exercici 1/02constants.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Constants</h1>
+    </body>
+<?php
+
+$num1 = 50;
+$num2 = 45;
+
+echo($num1 + $num2);
+?>
+
+</html>
